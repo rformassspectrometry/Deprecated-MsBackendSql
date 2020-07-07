6499eac047e901e2a9d0c40d922bd8782bee349a
plantton <plantton@gmail.com>
2020-07-07 19:06:01 +0200

    fix: show method

--- R/MsBackendSqlDb.R
+++ R/MsBackendSqlDb.R
@@ -114,7 +114,7 @@ setValidity("MsBackendSqlDb", function(object) {
 setMethod("show", "MsBackendSqlDb", function(object) {
     if (length(object@rows) == 0) {
         cat(class(object), "with", length(object@rows), "spectra\n")
-    } else if (length(object@rows) > 6) {
+    } else if (length(object@rows) > 10) {
       ## get the first 3 and last 3 rows, and print them
         columns <- c("msLevel", "rtime", "scanIndex")
         if (length(setdiff(columns, object@columns)) == 0) {
