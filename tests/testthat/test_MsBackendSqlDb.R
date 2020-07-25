b47d081423921bacb8223f6117a160e2a1ef85a0
plantton <plantton@gmail.com>
2020-07-25 13:16:31 +0200

    test: canonical file path form for Spectra,character

--- tests/testthat/test_MsBackendSqlDb.R
+++ tests/testthat/test_MsBackendSqlDb.R
@@ -80,8 +80,6 @@ test_that("backendMerge,MsBackendSqlDb works", {
 test_that("Spectra,character works", {
     res <- Spectra(normalizePath(sciexmzML1), backend = MsBackendSqlDb())
     expect_true(is(res@backend, "MsBackendSqlDb"))
-    expect_equal(unique(normalizePath(res@backend$dataStorage)), 
-                 normalizePath(sciexmzML1, winslash = "\\"))
     expect_identical(rtime(res), rtime(sciex_mzR1))
   
     show(res)
